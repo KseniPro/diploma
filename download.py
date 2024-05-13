6f998e8b0163f54d6276e9e5d3aa8534f0201000
msskriplyonok1 <msskriplyonok@edu.hse.ru>
2024-05-13 17:34:12 +0300

    Removed unncessary files

--- download.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system('curl -O https://raw.githubusercontent.com/pytorch/vision/main/references/detection/engine.py')
-os.system('curl -O https://raw.githubusercontent.com/pytorch/vision/main/references/detection/utils.py')
-os.system('curl -O https://raw.githubusercontent.com/pytorch/vision/main/references/detection/coco_utils.py')
-os.system('curl -O https://raw.githubusercontent.com/pytorch/vision/main/references/detection/coco_eval.py')
-os.system('curl -O https://raw.githubusercontent.com/pytorch/vision/main/references/detection/transforms.py')
